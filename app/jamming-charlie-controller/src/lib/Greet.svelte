bec5a0edd92c6e6abb72bc8391bfccbfcddad0b9
Tomas Lapes <tom@x-com.cz>
2024-05-09 19:58:51 +0200

    Initialize Tauri project with Svelte

---
+++ app/jamming-charlie-controller/src/lib/Greet.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { invoke } from "@tauri-apps/api/tauri";
+	import { open } from "@tauri-apps/api/dialog";
+
+	let name = "";
+	let greetMsg = "";
+
+	async function greet() {
+		// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+		// greetMsg = await invoke("greet", { name })
+		const selected = await open({
+			multiple: true,
+			filters: [
+				{
+					name: "Image",
+					extensions: ["png", "jpeg"],
+				},
+			],
+		});
+    console.log(selected)
+	}
+</script>
+
+<div>
+	<form class="row" on:submit|preventDefault={greet}>
+		<input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+		<button type="submit">Greet</button>
+	</form>
+	<p>{greetMsg}</p>
+</div>
